8d42f439eee7c9c94047aa6a63b97d1dd55cdf63
Tom Dowrick <tdowrick@gmail.com>
2020-06-05 11:58:33 +0100

    Don't use cmic cdash for test results

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -20,10 +20,4 @@ if(NOT DEFINED CTEST_DROP_METHOD)
   set(CTEST_DROP_METHOD "http")
 endif(NOT DEFINED CTEST_DROP_METHOD)
 
-if(CTEST_DROP_METHOD STREQUAL "http")
-  set(CTEST_DROP_SITE "cdash.cmiclab.cs.ucl.ac.uk")
-  set(CTEST_DROP_LOCATION "/submit.php?project=scikit-surgerygoicp")
-  set(CTEST_DROP_SITE_CDASH TRUE)
-endif(CTEST_DROP_METHOD STREQUAL "http")
-
 
